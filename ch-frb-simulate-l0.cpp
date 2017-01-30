9c7d74055e74c2e0f0c3507fdf3d53c9c604d845
Dustin Lang <dstndstn@gmail.com>
2017-01-30 11:04:11 -0500

    test a bunch of different random number generating schemes

--- ch-frb-simulate-l0.cpp
+++ ch-frb-simulate-l0.cpp
@@ -9,6 +9,7 @@
 // to simulate anything interesting (even Gaussian noise) at the full CHIME data rate,
 // and fixing this is a to-do item (see discussion in ch_frb_io/README).
 
+#include <sys/resource.h>
 #include <random>
 #include <iostream>
 #include <cstring>
@@ -60,7 +61,7 @@ public:
     Gaussian(double mean=0.0, double stddev=1.0) :
         _n_uni(0),
         _uni(-1.0, 1.0), _mean(mean), _stddev(stddev),
-        _have_y2(false)
+        _have_y2(false), _y2(0)
     {}
 
     template< class Generator >
@@ -164,6 +165,7 @@ int main(int argc, char **argv)
     
     float t0;
     uint_fast32_t sum;
+    double fsum;
 
     t0 = cpu_time();
     sum = 0;
@@ -216,14 +218,52 @@ int main(int argc, char **argv)
     cout << "CPU time: ranlux(): " << (cpu_time() - t0) << endl;
     cout << "  sum " << sum << endl;
 
+    std::ranlux48_base ranlux48b(seed);
+    t0 = cpu_time();
+    sum = 0;
+    for (int i=0; i<N; i++) {
+        sum += ranlux48b();
+    }
+    cout << "CPU time: ranlux48b(): " << (cpu_time() - t0) << endl;
+    cout << "  sum " << sum << endl;
+
+    /*
+    std::ranlux24 ranlux24(seed);
+    t0 = cpu_time();
+    sum = 0;
+    for (int i=0; i<N; i++) {
+        sum += ranlux24();
+    }
+    cout << "CPU time: ranlux24(): " << (cpu_time() - t0) << endl;
+    cout << "  sum " << sum << endl;
+
+    std::ranlux48 ranlux48(seed);
+    t0 = cpu_time();
+    sum = 0;
+    for (int i=0; i<N; i++) {
+        sum += ranlux48();
+    }
+    cout << "CPU time: ranlux48(): " << (cpu_time() - t0) << endl;
+    cout << "  sum " << sum << endl;
+    */
+
+    std::knuth_b knuth(seed);
+    t0 = cpu_time();
+    sum = 0;
+    for (int i=0; i<N; i++) {
+        sum += knuth();
+    }
+    cout << "CPU time: knuth(): " << (cpu_time() - t0) << endl;
+    cout << "  sum " << sum << endl;
+
     std::normal_distribution<> dist;
     t0 = cpu_time();
-    float fsum = 0.0;
+    fsum = 0.0;
     for (int i=0; i<N; i++) {
         fsum += dist(rng);
     }
     cout << "CPU time: normal(rng): " << (cpu_time() - t0) << endl;
-    cout << "  sum " << sum << endl;
+    cout << "  sum " << fsum << endl;
 
     Gaussian g;
     g._n_uni = 0;
@@ -233,7 +273,7 @@ int main(int argc, char **argv)
         fsum += g(rng);
     }
     cout << "CPU time: Gaussian(rng): " << (cpu_time() - t0) << endl;
-    cout << "  sum " << sum << endl;
+    cout << "  sum " << fsum << endl;
     cout << "N uniform rng calls: " << g._n_uni << endl;
 
     t0 = cpu_time();
@@ -243,7 +283,7 @@ int main(int argc, char **argv)
         fsum += g(rand1);
     }
     cout << "CPU time: Gaussian(rand_1): " << (cpu_time() - t0) << endl;
-    cout << "  sum " << sum << endl;
+    cout << "  sum " << fsum << endl;
     cout << "N uniform rng calls: " << g._n_uni << endl;
 
     t0 = cpu_time();
@@ -252,7 +292,7 @@ int main(int argc, char **argv)
         fsum += dist(rand1);
     }
     cout << "CPU time: normal(rand_1): " << (cpu_time() - t0) << endl;
-    cout << "  sum " << sum << endl;
+    cout << "  sum " << fsum << endl;
 
     std::uniform_real_distribution<> udist;
     t0 = cpu_time();
@@ -261,6 +301,48 @@ int main(int argc, char **argv)
         fsum += udist(rng);
     }
     cout << "CPU time: uniform(rng): " << (cpu_time() - t0) << endl;
+    cout << "  sum " << fsum << endl;
+
+    std::uniform_int_distribution<short> shuni;
+
+    t0 = cpu_time();
+    sum = 0;
+    for (int i=0; i<N; i++) {
+      sum += shuni(rng);
+    }
+    cout << "CPU time: uniform_short(rng): " << (cpu_time() - t0) << endl;
+    cout << "  sum " << sum << endl;
+
+    std::uniform_int_distribution<int> iuni;
+
+    t0 = cpu_time();
+    sum = 0;
+    for (int i=0; i<N; i++) {
+      sum += iuni(rng);
+    }
+    cout << "CPU time: uniform_int(rng): " << (cpu_time() - t0) << endl;
+    cout << "  sum " << sum << endl;
+
+    std::discrete_distribution<int> dd4( { 0.3, 0.5, 0.7, 0.9 } );
+
+    t0 = cpu_time();
+    sum = 0;
+    for (int i=0; i<N; i++) {
+      sum += dd4(rng);
+    }
+    cout << "CPU time: dd4: " << (cpu_time() - t0) << endl;
+    cout << "  sum " << sum << endl;
+
+    // mu = 127.5, s = 30., x = np.arange(256)
+    // '{' + ','.join(['%.6f' % f for f in np.exp(-0.5 * (x - mu)**2 / s**2)]) + '}'
+    std::discrete_distribution<int> dd256({0.000120,0.000138,0.000158,0.000182,0.000209,0.000240,0.000274,0.000314,0.000359,0.000409,0.000467,0.000531,0.000604,0.000687,0.000780,0.000884,0.001001,0.001132,0.001280,0.001444,0.001628,0.001834,0.002063,0.002319,0.002603,0.002918,0.003268,0.003656,0.004086,0.004561,0.005086,0.005665,0.006303,0.007004,0.007775,0.008622,0.009550,0.010566,0.011677,0.012891,0.014215,0.015657,0.017227,0.018934,0.020786,0.022794,0.024969,0.027320,0.029860,0.032599,0.035550,0.038726,0.042138,0.045800,0.049725,0.053926,0.058418,0.063213,0.068326,0.073770,0.079560,0.085708,0.092230,0.099137,0.106444,0.114162,0.122303,0.130880,0.139903,0.149382,0.159326,0.169743,0.180640,0.192023,0.203897,0.216265,0.229129,0.242487,0.256340,0.270683,0.285512,0.300818,0.316593,0.332825,0.349501,0.366604,0.384118,0.402021,0.420292,0.438905,0.457833,0.477048,0.496516,0.516206,0.536080,0.556101,0.576229,0.596423,0.616639,0.636832,0.656956,0.676963,0.696805,0.716432,0.735794,0.754840,0.773519,0.791780,0.809572,0.826844,0.843548,0.859633,0.875052,0.889758,0.903707,0.916855,0.929162,0.940588,0.951097,0.960656,0.969233,0.976801,0.983335,0.988813,0.993218,0.996534,0.998751,0.999861,0.999861,0.998751,0.996534,0.993218,0.988813,0.983335,0.976801,0.969233,0.960656,0.951097,0.940588,0.929162,0.916855,0.903707,0.889758,0.875052,0.859633,0.843548,0.826844,0.809572,0.791780,0.773519,0.754840,0.735794,0.716432,0.696805,0.676963,0.656956,0.636832,0.616639,0.596423,0.576229,0.556101,0.536080,0.516206,0.496516,0.477048,0.457833,0.438905,0.420292,0.402021,0.384118,0.366604,0.349501,0.332825,0.316593,0.300818,0.285512,0.270683,0.256340,0.242487,0.229129,0.216265,0.203897,0.192023,0.180640,0.169743,0.159326,0.149382,0.139903,0.130880,0.122303,0.114162,0.106444,0.099137,0.092230,0.085708,0.079560,0.073770,0.068326,0.063213,0.058418,0.053926,0.049725,0.045800,0.042138,0.038726,0.035550,0.032599,0.029860,0.027320,0.024969,0.022794,0.020786,0.018934,0.017227,0.015657,0.014215,0.012891,0.011677,0.010566,0.009550,0.008622,0.007775,0.007004,0.006303,0.005665,0.005086,0.004561,0.004086,0.003656,0.003268,0.002918,0.002603,0.002319,0.002063,0.001834,0.001628,0.001444,0.001280,0.001132,0.001001,0.000884,0.000780,0.000687,0.000604,0.000531,0.000467,0.000409,0.000359,0.000314,0.000274,0.000240,0.000209,0.000182,0.000158,0.000138,0.000120});
+
+    t0 = cpu_time();
+    sum = 0;
+    for (int i=0; i<N; i++) {
+      sum += dd256(rng);
+    }
+    cout << "CPU time: dd256: " << (cpu_time() - t0) << endl;
     cout << "  sum " << sum << endl;
 
     return 0;
