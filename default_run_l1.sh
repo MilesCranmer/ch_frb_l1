45ae3ff2c5bee0d8b725dbe0ad8d4146e1a16733
Shiny Brar <charanjotbrar@gmail.com>
2017-10-21 03:18:43 -0400

    Better date format for logs

--- default_run_l1.sh
+++ default_run_l1.sh
@@ -1,1 +1,1 @@
-./ch-frb-l1 l1_configs/l1_example3.yaml rfi_configs/rfi_placeholder.json bonsai_configs/bonsai_noups_nbeta1.txt L1b_config.yaml 2>&1 | tee l1."$(date +'%T')".log
+./ch-frb-l1 l1_configs/l1_example3.yaml rfi_configs/rfi_placeholder.json bonsai_configs/bonsai_noups_nbeta1.txt L1b_config.yaml 2>&1 | tee l1."$(date +'%H%M%S-%Y%m%d')".log
