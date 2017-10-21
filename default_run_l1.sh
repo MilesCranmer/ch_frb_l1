f487021f3bfcfb0cc2734898fdba88745e404a06
Shiny Brar <charanjotbrar@gmail.com>
2017-10-21 05:34:24 -0400

    Add a file to check launch status, then break
    - Good for looping

--- default_run_l1.sh
+++ default_run_l1.sh
@@ -3,3 +3,14 @@ echo $$ > /tmp/default_run_l1.sh
 nohup ./launch_l1.sh &
 sleep 60
 nohup ssh pipeline@frb-compute-0 'cd ~/miles/ch_frb_l1 && source activate chime-frb && timeout 800 ./default_run_sim.sh' &
+i=0
+while [ "$(cat /tmp/l1_launch_status.txt)" == "1" ]; do
+    sleep 5
+    echo "Waiting..."
+    i=$((i+1))
+    if [[ "$i" -gt 200 ]]; then
+        echo "Broke"
+        exit 0
+    fi
+done
+echo "Finished iterations"
