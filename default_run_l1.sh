f8cc8ac59bc8cf3c2ba809592e47276749ead90b
Shiny Brar <charanjotbrar@gmail.com>
2017-10-21 03:16:28 -0400

    Update run configuration on frb-compute-1

--- default_run_l1.sh
+++ default_run_l1.sh
@@ -1,1 +1,1 @@
-nohup ./ch-frb-l1 l1_configs/l1_low_ram.yaml rfi_configs/rfi_placeholder.json bonsai_configs/bonsai_example1.txt l1b_placeholder_file > l1.log &
+./ch-frb-l1 l1_configs/l1_example3.yaml rfi_configs/rfi_placeholder.json bonsai_configs/bonsai_noups_nbeta1.txt L1b_config.yaml 2>&1 | tee l1."$(date +'%T')".log
