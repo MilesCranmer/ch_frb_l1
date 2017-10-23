5e96614f427194645c34bf98493ee489fa2fc89b
MilesCranmer <miles.cranmer@gmail.com>
2017-10-23 17:48:13 -0400

    Fix new frb params not going to right file

--- default_run_sim.sh
+++ default_run_sim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo $$ > /tmp/default_run_sim.pid
-./generate_pulse_params.py > newbeams.dt
+./generate_pulse_params.py > newbeams.dat
 ./ch-frb-simulate-l0 l0_configs/l0_example3.yaml 300 newbeams.dat | tee sim.$(date +'%H%M%S-%Y%m%d').log
