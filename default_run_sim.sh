ab2b8565cc061f64dffce3ea7811cdc91cb3f214
MilesCranmer <miles.cranmer@gmail.com>
2017-10-25 13:21:45 -0400

    Start using datelabel tag on simulator

--- default_run_sim.sh
+++ default_run_sim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo $$ > /tmp/default_run_sim.pid
+DATELABEL=$1
 ./generate_pulse_params.py > newbeams.dat
-./ch-frb-simulate-l0 l0_configs/l0_example3.yaml 300 newbeams.dat | tee sim.$(date +'%H%M%S-%Y%m%d').log
+./ch-frb-simulate-l0 l0_configs/l0_example3.yaml 300 newbeams.dat | tee sim."$DATELABEL".log
