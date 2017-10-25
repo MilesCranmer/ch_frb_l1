c7deee4dffb330035c5b4e484be821871cd63c1c
Shiny Brar <charanjotbrar@gmail.com>
2017-10-25 13:23:41 -0400

    Use Datelabel tag

--- default_run_l1.sh
+++ default_run_l1.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-echo $$ > /tmp/default_run_l1.sh
-nohup ./launch_l1.sh &
+echo $$ > /tmp/default_run_l1.pid
+DATELABEL="$(date +'%H%M%S-%Y%m%d')"
+./launch_l1.sh $DATELABEL &
 sleep 60
-nohup ssh pipeline@frb-compute-0 'cd ~/miles/ch_frb_l1 && source activate chime-frb && timeout 800 ./default_run_sim.sh' &
+ssh pipeline@frb-compute-0 "cd ~/miles/ch_frb_l1 && source activate chime-frb && timeout 800 ./default_run_sim.sh $DATELABEL" &
 i=0
 while [ "$(cat /tmp/l1_launch_status.txt)" == "1" ]; do
     sleep 5
     echo "Waiting..."
     i=$((i+1))
-    if [[ "$i" -gt 200 ]]; then
+    if [[ "$i" -gt 180 ]]; then
         echo "Broke"
+	kill -9 `cat /tmp/launch_l1.pid`
+	ssh pipeline@frb-compute-0 'kill -9 `cat /tmp/default_run_sim.sh`'
         exit 0
     fi
 done
--- launch_l1.sh
+++ launch_l1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo $$ > /tmp/launch_l1.pid
+DATELABEL=$1
 echo '1' > /tmp/l1_launch_status.txt
-timeout 1000 ./ch-frb-l1 l1_configs/l1_example3.yaml rfi_configs/rfi_placeholder.json bonsai_configs/bonsai_noups_nbeta1.txt L1b_config.yaml 2>&1 | tee l1."$(date +'%H%M%S-%Y%m%d')".log
+timeout 1000 ./ch-frb-l1 l1_configs/l1_example3.yaml rfi_configs/rfi_placeholder.json bonsai_configs/bonsai_noups_nbeta1.txt L1b_config.yaml 2>&1 | tee l1."$DATELABEL".log
 echo '0' > /tmp/l1_launch_status.txt
